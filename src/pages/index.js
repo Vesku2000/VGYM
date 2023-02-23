e9fdb927d660d8c8a8711209467ba9f9bd7a97ec
Vesku2000 <valtterisyrjanen@gmail.com>
2023-02-23 10:52:33 +0200

    2 new task done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,6 @@
+import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup: Vaskiluoto',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/0/02/Vasklot_hamn_cirka_1994_med_Silja_Festival_p%C3%A5_inkommande_och_Wasa_Queen_vid_kaj_i_bakgrunden_Sundom_sk%C3%A4rg%C3%A5rd.jpg',
-    address: 'Vaskiluoto, Vaasa',
-    description: 'This is a first meetup! Picture: Maritime Museum of Finland, CC BY-SA 4.0',
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup: VAMK',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/8/88/Vaasan_ammattikorkeakoulu_2018.jpg',
-    address: 'Wolffintie 30, Vaasa',
-    description: 'This is a second meetup! Picture: Santeri Viinamäki, 2018m CC BY-SA 4.0',
-  },
-];
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
@@ -25,9 +8,25 @@ function HomePage(props) {
 
 export async function getStaticProps() {
   // fetch data from an API
+  const client = await MongoClient.connect(
+    'mongodb+srv://testi:testi@cluster0.xcwm9pa.mongodb.net/meetups_db?retryWrites=true&w=majority'
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
   return {
     props: {
-      meetups: DUMMY_MEETUPS
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      }))
     },
     revalidate: 1
   }; 
