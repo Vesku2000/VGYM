b6a878069540e9e7c441e4a01e0d4d2e0c4f9de8
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-02-24 17:19:00 +0200

    adding images

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,30 +1,34 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
 
 function NewMeetupForm(props) {
   const titleInputRef = useRef();
-  const imageInputRef = useRef();
   const motivationInputRef = useRef();
   const descriptionInputRef = useRef();
+
+  const [selectedImage, setSelectedImage] = useState('chestImg');
 
   function submitHandler(event) {
     event.preventDefault();
 
     const enteredTitle = titleInputRef.current.value;
-    const enteredImage = imageInputRef.current.value;
     const enteredMotivation = motivationInputRef.current.value;
     const enteredDescription = descriptionInputRef.current.value;
 
     const meetupData = {
       title: enteredTitle,
-      image: enteredImage,
+      image: selectedImage,
       motivation: enteredMotivation,
       description: enteredDescription,
     };
 
     props.onAddMeetup(meetupData);
+  }
+
+  function handleImageChange(event) {
+    setSelectedImage(event.target.value);
   }
 
   return (
@@ -36,7 +40,12 @@ function NewMeetupForm(props) {
         </div>
         <div className={classes.control}>
           <label htmlFor='image'>Exercise Image</label>
-          <input type='url' required id='image' ref={imageInputRef} />
+          <select value={selectedImage} onChange={handleImageChange}>
+            <option value='https://www.muscleandfitness.com/wp-content/uploads/2015/10/cable-crossover-content-chest-pec.jpg?quality=86&strip=all'>Chest Image</option>
+            <option value='https://www.muscleandfitness.com/wp-content/uploads/2019/12/1109-Man-Posing-Biceps-shutterstock_553440583.jpg?quality=86&strip=all'>Arm Image</option>
+            <option value='https://cdn.pixabay.com/photo/2021/01/04/06/21/man-5886580_960_720.jpg'>Leg Image</option>
+            <option value='https://i.ytimg.com/vi/gey73xiS8F4/maxresdefault.jpg'>Back Image</option>
+          </select>
         </div>
         <div className={classes.control}>
           <label htmlFor='address'>Fiilis</label>
