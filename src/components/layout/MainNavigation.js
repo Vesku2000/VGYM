776c2594ca92cd3e6bb822965aa31d054d9e5f84
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-02-25 13:31:16 +0200

    hamburger menu added

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import Link from 'next/link';
 
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenuHandler = () => {
+    setIsMenuOpen(prevState => !prevState);
+  };
 
   return (
     <header className={classes.header}>
       <div className={classes.logo}>Treeni päiväkirja</div>
       <nav>
-        <ul>
+        <div className={classes.hamburger} onClick={toggleMenuHandler}>
+          <div className={`${classes.bar} ${isMenuOpen ? classes.close : ''}`} />
+          <div className={`${classes.bar} ${isMenuOpen ? classes.close : ''}`} />
+          <div className={`${classes.bar} ${isMenuOpen ? classes.close : ''}`} />
+        </div>
+        <ul className={`${classes.navItems} ${isMenuOpen ? classes.show : ''}`}>
           <li>
             <Link href='/'>Kaikki Treenit</Link>
           </li>
