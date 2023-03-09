1a6857103def3c41b268b0deac5e3bc4c3fbdd7b
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-03-09 13:59:54 +0200

    still not working

--- src/components/meetups/MeetupDetail.js
+++ src/components/meetups/MeetupDetail.js
@@ -11,11 +11,34 @@ function MeetupDetail(props) {
   function submitHandler(event) {
     event.preventDefault();
     const id = props.id;
-    deleteTrainingHandler(id);
+    console.log(id);
+  
+    fetch('/api/new-meetup', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: id }),
+    })
+    
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Deleting meetup failed.');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.message);
+        // TODO: handle successful deletion
+      })
+      .catch((error) => {
+        console.error(error);
+        // TODO: handle error
+      });
   }
 
   return (
-    <section className={classes.detail}>
+    <form className={classes.detail} onSubmit={submitHandler}>
       <img src={props.image} alt={props.title} />
       <h1>{props.title}</h1>
       <h1>{props.date}</h1>
@@ -24,10 +47,10 @@ function MeetupDetail(props) {
       <p>{props.description}</p>
       <p>{props.id}</p>
       <div className={classes.actions}>
-        <button onClick={submitHandler}>Poista treeni</button>
+        <button>Poista treeni</button>
       </div>
       <button>Muokkaa treeniä</button>
-    </section>
+    </form>
   );
 }
 
--- src/pages/api/new-meetups.js
+++ src/pages/api/new-meetups.js
@@ -1,6 +1,9 @@
 
-
 import { connectDatabase, insertDocument, deleteDocument } from '../../helpers/db-util';
+import { ObjectId } from 'mongodb';
+
+// rest of the code...
+
 // /api/new-meetup
 // POST /api/new-meetup
 
@@ -39,10 +42,13 @@ async function handler(req, res) {
         }
 
         try {
-            await deleteDocument(client, 'exercises', data);
-            
-            client.close();
-            res.status(200).json({ message: 'exercise deleted'});
+             async function deleteDocument(client, collection, id) {
+                const db = client.db();
+                const result = await db.collection(collection).deleteOne({ _id: new ObjectId(id) });
+                console.log(`${result.deletedCount} document(s) deleted.`);
+                return result;
+              }
+              
         } catch (error) {
             res.status(500).json({ message: 'Deleting data failed'});
             return;
