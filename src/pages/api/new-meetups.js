eed4c894e4049316ae611026bd79c7c949ee7aaa
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-03-14 21:32:02 +0200

    deleting unused code

--- src/pages/api/new-meetups.js
+++ src/pages/api/new-meetups.js
@@ -29,30 +29,6 @@ async function handler(req, res) {
         }
 
         res.status(201).json({ message: 'exercise inserted'});
-    } else if (req.method === 'DELETE') {
-        const data = req.body;
-
-        let client;
-
-        try {
-            client = await connectDatabase();
-        } catch(error) {
-            res.status(500).json({ message: 'Connecting to the database failed'});
-            return;
-        }
-
-        try {
-             await deleteDocumentById(client, 'exercises', data) {
-                const db = client.db();
-                const result = await db.collection(collection).deleteOne({ _id: new ObjectId(id) });
-                console.log(`${result.deletedCount} document(s) deleted.`);
-                return result;
-              }
-              
-        } catch (error) {
-            res.status(500).json({ message: 'Deleting data failed'});
-            return;
-        }
     }
 }
 
